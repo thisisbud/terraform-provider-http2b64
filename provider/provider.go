3d13d07d52c32128dfa92bd5875295f5830e026f
Josh Harwood <josh.harwood@thisisbud.com>
2025-04-24 14:07:54 +0100

    Update to new framework and try deal with caching issues

--- provider/provider.go
+++ provider/provider.go
@@ -1,32 +1,82 @@
+//package provider
+//
+//import (
+//	"context"
+//	"github.com/hashicorp/terraform-plugin-framework/datasource"
+//	"github.com/hashicorp/terraform-plugin-framework/provider"
+//
+//	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+//	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+//)
+//
+//func New(version string) func() *schema.Provider {
+//	return func() *schema.Provider {
+//		p := &schema.Provider{
+//
+//			ResourcesMap: map[string]*schema.Resource{
+//				"http2b64": resourcehttp2b64(),
+//			},
+//		}
+//
+//		p.ConfigureContextFunc = configure(version, p)
+//
+//		return p
+//	}
+//}
+//
+//type apiClient struct {
+//}
+//
+//func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+//	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+//		return &apiClient{}, nil
+//	}
+//}
+
+// Break
+
 package provider
 
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	_ "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	_ "github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func New(version string) func() *schema.Provider {
-	return func() *schema.Provider {
-		p := &schema.Provider{
+var _ provider.Provider = (*http2b64Provider)(nil)
 
-			ResourcesMap: map[string]*schema.Resource{
-				"http2b64": resourcehttp2b64(),
-			},
-		}
+//var _ provider.ProviderWithMetadata = (*http2b64Provider)(nil)
 
-		p.ConfigureContextFunc = configure(version, p)
+type http2b64Provider struct{}
 
-		return p
+func New() func() provider.Provider {
+	return func() provider.Provider {
+		return &http2b64Provider{}
 	}
 }
 
-type apiClient struct {
+func (p *http2b64Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 }
 
-func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		return &apiClient{}, nil
+func (p *http2b64Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "thisisbud"
+}
+
+func (p *http2b64Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		//NewDataSource,
 	}
+}
+
+func (p *http2b64Provider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewResource,
+	}
+}
+
+func (p *http2b64Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 }
